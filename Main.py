acdeb4c15f59e1465a0857f549cbf32474d5cb6d
Rafael Henriques Aquino Correa <7591rafa@gmail.com>
2025-11-18 15:40:55 -0300

    Criação de funções - Melhoria na organização
    Refactor code to modularize audio downloading, transcription, and summarization.

--- Main.py
+++ Main.py
@@ -1,25 +1,31 @@
-
 import whisper
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 from summarizer import Summarizer
 
+def baixar_audio(url):
+    yt = YouTube(url, on_progress_callback=on_progress)
+    print(yt.title)
+    audio = yt.streams.get_audio_only().download()
+    return audio
+
+def transcrever_audio(arquivo, modelo):
+    dicionario = modelo.transcribe(arquivo)
+    return dicionario["text"]
+
+def gerar_resumo(texto):
+    modelo_resumo = Summarizer()
+    return modelo_resumo(texto, min_length=30, max_length=1000)
+
 url = input("Escreva a url do video do youtube: ")
 
-yt = YouTube(url, on_progress_callback=on_progress)
-print(yt.title)
-
-ys = yt.streams.get_audio_only()
-arquivo = ys.download()
+arquivo = baixar_audio(url)
 
 modelo = whisper.load_model("base")
 
-dicionario = modelo.transcribe(arquivo)
-transcricao = dicionario['text']
+transcricao = transcrever_audio(arquivo, modelo)
 
+resumo = gerar_resumo(transcricao)
 
-texto_completo = transcricao
-modelo = Summarizer()
-resumo = modelo(texto_completo, min_length=30, max_length=1000)
-
+print("\n RESUMO: \n")
 print(resumo)
